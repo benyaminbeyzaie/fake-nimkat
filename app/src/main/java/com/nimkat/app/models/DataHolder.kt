ff37b8a6d73e15da0f354e4b7f6d970d99974481
Benyamin Beyzaie <benyamin.beyzaie@hezardastan.net>
2022-07-14 14:22:23 +0430

    create data holder class

---
+++ app/src/main/java/com/nimkat/app/models/DataHolder.kt
@@ -0,0 +1,15 @@
+package com.nimkat.app.models
+
+class DataHolder<T>(val data: T?, val message: String?, val status: DataStatus) {
+
+    companion object {
+        fun <T> error(message: String? = "Error!"): DataHolder<T> = DataHolder(null, message, DataStatus.Error)
+        fun <T> success(data: T): DataHolder<T> = DataHolder(data, null, DataStatus.Success)
+        fun <T> loading(message: String? = "Loading..."): DataHolder<T> = DataHolder(null, message, DataStatus.Loading)
+        fun <T> pure(): DataHolder<T> = DataHolder(null, null, DataStatus.Pure)
+    }
+}
+
+enum class DataStatus {
+    Pure, Error, Loading, Success
+}
