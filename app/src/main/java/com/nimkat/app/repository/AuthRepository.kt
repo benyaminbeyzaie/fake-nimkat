aee28383e9380375e32421567983448da48ab757
Benyamin Beyzaie <benyamin.beyzaie@hezardastan.net>
2022-07-17 15:36:11 +0430

    add clear auth and fix functionality

--- app/src/main/java/com/nimkat/app/repository/AuthRepository.kt
+++ app/src/main/java/com/nimkat/app/repository/AuthRepository.kt
@@ -1,33 +1,42 @@
 package com.nimkat.app.repository
 
+import android.util.Log
+import com.google.gson.Gson
 import com.nimkat.app.api.NimkatApi
 import com.nimkat.app.models.AuthModel
 import com.nimkat.app.models.GetCodeBody
 import com.nimkat.app.models.GetCodeResponse
 import com.nimkat.app.models.VerifyCodeBody
+import com.nimkat.app.utils.AuthPrefs
 import retrofit2.Response
 import javax.inject.Inject
+import javax.inject.Singleton
 
-class AuthRepository @Inject constructor(private val api: NimkatApi) {
-
-    private var _lastCalledId = ""
+@Singleton
+class AuthRepository @Inject constructor(
+    private val api: NimkatApi,
+    private val authPrefs: AuthPrefs
+) {
+    fun initAuth(): AuthModel? {
+        val authString = authPrefs.getAuthString()
+        if (authString === null) return null
+        val gson = Gson()
+        return gson.fromJson(authString, AuthModel::class.java)
+    }
 
     suspend fun getCode(phoneNumber: String): Response<GetCodeResponse> {
-        val apiResponse = api.getCode(GetCodeBody(phoneNumber, "-"))
-        if (apiResponse.isSuccessful) {
-            _lastCalledId = apiResponse.body()!!.smsCode.toString()
-        }
-        return apiResponse
-
+        return api.getCode(GetCodeBody(phoneNumber, "-"))
     }
 
-    suspend fun verifyCode(smsCode: String): Response<AuthModel>? {
-        if (_lastCalledId.isEmpty()) {
-            return null
-        }
-        val apiResponse = api.verifyCode(_lastCalledId, VerifyCodeBody(smsCode))
-        _lastCalledId = ""
+    suspend fun verifyCode(smsCode: String, id: String): Response<AuthModel>? {
+        val apiResponse = api.verifyCode(id, VerifyCodeBody(smsCode))
+        if (apiResponse.body() === null) return null;
+        val gson = Gson()
+        authPrefs.setAuthString(gson.toJson(apiResponse.body()))
         return apiResponse
     }
 
+    fun clearAuth(){
+        authPrefs.clearAuth()
+    }
 }
