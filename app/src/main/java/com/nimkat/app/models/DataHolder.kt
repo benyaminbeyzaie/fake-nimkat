feef98278278d3a7120d80a60eefd7202b3833dd
Benyamin Beyzaie <benyamin.beyzaie@hezardastan.net>
2022-07-21 23:59:51 +0430

    add loading next state to data holder

--- app/src/main/java/com/nimkat/app/models/DataHolder.kt
+++ app/src/main/java/com/nimkat/app/models/DataHolder.kt
@@ -5,6 +5,7 @@ class DataHolder<T>(val data: T?, val message: String?, val status: DataStatus) 
     companion object {
         fun <T> error(message: String? = "Error!"): DataHolder<T> = DataHolder(null, message, DataStatus.Error)
         fun <T> success(data: T): DataHolder<T> = DataHolder(data, null, DataStatus.Success)
+        fun <T> loadingNext(data: T?): DataHolder<T> = DataHolder(data, null, DataStatus.LoadingNext)
         fun <T> loading(message: String? = "Loading..."): DataHolder<T> = DataHolder(null, message, DataStatus.Loading)
         fun <T> pure(): DataHolder<T> = DataHolder(null, null, DataStatus.Pure)
         fun <T> needCompletion(data: T): DataHolder<T> = DataHolder(data, null, DataStatus.NeedCompletion)
@@ -12,5 +13,5 @@ class DataHolder<T>(val data: T?, val message: String?, val status: DataStatus) 
 }
 
 enum class DataStatus {
-    Pure, Error, Loading, Success , NeedCompletion
+    Pure, Error, Loading, LoadingNext, Success , NeedCompletion
 }
